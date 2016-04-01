7c957762992150bfcc26d75705b0ddf8909daeba
Leonardo Walcher <leonardo.walcher@gmail.com>
2016-04-01 11:28:36 +0200

    Final Version

--- Tutorial/ViewController.swift
+++ Tutorial/ViewController.swift
@@ -9,13 +9,72 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    //Properties
+    var maxTaps = 0
+    var currentTaps = 0
+    
+    //Outlets
     @IBOutlet weak var logoImg: UIImageView!
     @IBOutlet weak var howManyTapsTxt: UITextField!
     @IBOutlet weak var playBtn: UIButton!
     
     @IBOutlet weak var tapBtn: UIButton!
     @IBOutlet weak var tapsLbl: UILabel!
-
+    
+    @IBAction func onCoinTapped(sender: UIButton!) {
+        currentTaps += 1
+        updateTapsLable()
+        
+        if isgameOver() {
+            restartGame()
+        }
+        
+    }
+    
+    @IBAction func onPlayBtnPressed(sender: UIButton!) {
+        
+        if howManyTapsTxt.text != nil && howManyTapsTxt.text != "" {
+            
+            maxTaps = Int(howManyTapsTxt.text!)!
+            currentTaps = 0
+            
+            logoImg.hidden = true
+            playBtn.hidden = true
+            howManyTapsTxt.hidden = true
+            
+            tapBtn.hidden = false
+            tapsLbl.hidden = false
+            
+            updateTapsLable()
+        }
+    }
+    
+    func restartGame() {
+        maxTaps = 0
+        howManyTapsTxt.text = ""
+        
+        logoImg.hidden = false
+        playBtn.hidden = false
+        howManyTapsTxt.hidden = false
+        
+        tapBtn.hidden = true
+        tapsLbl.hidden = true
+        
+    }
+    
+    func isgameOver() -> Bool {
+        if currentTaps == maxTaps {
+            return true
+            
+        } else {
+            return false
+            
+        }
+    }
+    
+    func updateTapsLable() {
+        tapsLbl.text = "\(currentTaps) Taps"
+    }
 }
 
